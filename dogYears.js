e1a948443549ad600853e6c889da14cf66e1fe06
Cody Noel <code.codynoel@gmail.com>
2020-02-03 00:12:01 -0700

    Add files via upload

---
+++ dogYears.js
@@ -0,0 +1,19 @@
+// The age of the user
+let myAge = 27;
+// Early years
+let earlyYears = 2;
+
+earlyYears *= 10.5;
+// Finding the later years for age
+let laterYears = myAge - 2;
+// Finding the later years for dog years
+laterYears *= 4;
+// Getting calculation for age in dog years
+myAgeInDogYears = earlyYears + laterYears;
+// Taking name and making it lowercase
+myName = 'Cody'.toLowerCase();
+
+
+
+// Logging all of the variables into a string
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} old in dog years`);
